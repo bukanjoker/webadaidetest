e7adcbf7f225afe2376ed836a2eb931ac85a8546
Eka DDM <eka.p@deltadatamandiri.com>
2022-04-06 22:11:43 +0700

    feat: binding data

--- src/Components/programAvailable.js
+++ src/Components/programAvailable.js
@@ -3,9 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Badge from 'react-bootstrap/Badge'
+import ProgressBar from 'react-bootstrap/ProgressBar'
 
-
-const programAvailable = () => {
+const programAvailable = ({
+    title,
+    type,
+    progress,
+    dana,
+    sisaHari,
+}) => {
     return (
         <div className="vv">
             <Container className="comProgram">
@@ -15,10 +22,12 @@ const programAvailable = () => {
                     </Col>
                     <Col xs={6} className="ket">
                         <div className="tittle">
-                            Kurangi Sampah dari Planet Bumi
+                            {title}
                         </div>
-                        <img src="Img/kategori_umum.png" />
-                        <img src="Img/progress.png" />
+                        <Badge pill className={type.toLowerCase()}>
+                            {type}
+                        </Badge>
+                        <ProgressBar variant="warning" now={progress} />
                         <div className="minitable">
                             <Container>
                                 <Row className="slim">
@@ -31,10 +40,10 @@ const programAvailable = () => {
                                 </Row>
                                 <Row className="bold">
                                     <Col xs={7}>
-                                        Rp.502.397.992
+                                        Rp.{dana}
                                     </Col>
                                     <Col xs={5}>
-                                        382 hari
+                                        {sisaHari} hari
                                     </Col>
                                 </Row>
                             </Container>
